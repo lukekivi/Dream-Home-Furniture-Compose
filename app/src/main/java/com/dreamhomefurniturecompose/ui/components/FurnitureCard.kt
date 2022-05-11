2cf559d2925cff5450273a41d6a2a8e47c5e4a5a
lukekivi <lukekivi@gmail.com>
2022-05-10 22:57:57 -0500

    optimize imports

--- app/src/main/java/com/dreamhomefurniturecompose/ui/components/FurnitureCard.kt
+++ app/src/main/java/com/dreamhomefurniturecompose/ui/components/FurnitureCard.kt
@@ -1,7 +1,6 @@
 package com.dreamhomefurniturecompose.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -12,7 +11,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
